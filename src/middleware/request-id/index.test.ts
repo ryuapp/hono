dcedd029a812a6e1fd8ae1e6338e82deca380367
ryu <114303361+ryuapp@users.noreply.github.com>
2024-06-25 00:49:30 +0900

    feat(middleware): introduce Request ID middleware

---
+++ src/middleware/request-id/index.test.ts
@@ -0,0 +1,27 @@
+import { Hono } from '../../hono'
+import { requestID } from '.'
+
+describe('Request ID Middleware', () => {
+  const app = new Hono()
+
+  app.use(requestID())
+  app.get('/requestID', (c) => c.text(c.req.header('X-Request-ID') ?? 'No response'))
+
+  it('Should return random request id', async () => {
+    const res = await app.request('http://localhost/requestID')
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.body).not.toBe('No response')
+  })
+
+  it('Should return configured request id', async () => {
+    const res = await app.request('http://localhost/requestID', {
+      headers: {
+        'X-Request-ID': 'hono-is-cool',
+      },
+    })
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.body).toBe('hono-is-cool')
+  })
+})
---
+++ src/middleware/request-id/index.ts
@@ -0,0 +1,31 @@
+/**
+ * @module
+ * Request ID Middleware for Hono.
+ */
+
+import type { MiddlewareHandler } from '../../types'
+
+/**
+ * Request ID Middleware for Hono.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(requestID())
+ * app.get('/', (c) => {
+ *   console.log(c.req.header('X-Request-ID')) // Debug
+ *   return c.text('Hello World!')
+ * })
+ * ```
+ */
+export const requestID = (): MiddlewareHandler => {
+  return async function requestID(c, next) {
+    const requestID = c.req.header('X-Request-ID') ?? crypto.randomUUID()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    c.req.raw.headers['X-Request-ID'] = requestID
+    await next
+  }
+}
