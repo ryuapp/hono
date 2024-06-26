48e93651390db25dccfc7ca743efc599f838c965
ryu <114303361+ryuapp@users.noreply.github.com>
2024-06-26 23:20:06 +0900

    update jsdoc

--- src/middleware/request-id/index.ts
+++ src/middleware/request-id/index.ts
@@ -14,6 +14,14 @@ export type RequesIdOptions = {
 
 /**
  * Request ID Middleware for Hono.
+ *
+ * @param {object} options - Options for Request ID middleware.
+ * @param {number} [options.maxLength=255] - A maximum length of request id.
+ * If a value retrieved from headers exceeds the length, it will be cut to the length.
+ * @param {string} [options.headerName=X-Request-Id] - A header name.
+ * @param {string} [options.variableName=requestId] - A variable name used in Variables.
+ * @param {generator} [options.generator=crypto.randomUUID()] - A request id generation function
+ *
  * @returns {MiddlewareHandler} The middleware handler function.
  *
  * @example
@@ -34,7 +42,6 @@ export type RequesIdOptions = {
  */
 export const requestId = (options?: RequesIdOptions): MiddlewareHandler => {
   return async function requestId(c, next) {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
     const maxLength = options?.maxLength ?? 255
     const headerName = options?.headerName ?? 'X-Request-Id'
     const requestId = c.req.header(headerName) ?? options?.generator() ?? crypto.randomUUID()
